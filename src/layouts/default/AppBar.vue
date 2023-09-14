81cbcd0e85662359822c2caabf8d4abbb41798ef
mperezdegracia <mperezdegracia@itba.edu.ar>
2023-09-14 10:42:34 -0300

    project creation

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar color="white">
+      <v-img src="../../assets/logo_omg.png" max-height="100" max-width="100"></v-img>
+
+    <template v-slot:append>
+      <v-btn variant="tonal" color="primary" class="mx-10">Login</v-btn>
+    </template>
+
+  </v-app-bar>
+</template>
+
+<script setup>
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-contrast min fill-height">
+    <div class="pt-10 d-flex justify-center">
+      <Exercise class=""></Exercise>
+      <h1 class="text-h1">Start building your workout routines.</h1>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+  import Exercise  from '@/components/Exercise.vue'
+</script>
