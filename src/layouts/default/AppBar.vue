a25f87d783f49bf82c7e8716ccdebd4e33c64f54
mperezdegracia <mperezdegracia@itba.edu.ar>
2023-09-22 16:42:09 -0300

    navbar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,39 @@
 <template>
-  <v-app-bar color="white">
-      <v-img src="../../assets/logo_omg.png" max-height="100" max-width="100"></v-img>
+  <v-app-bar color="white" app>
+    <!-- Logo/Branding -->
+    <a href="#">
+      <img src="@/assets/logo_omg.png" alt="Logo" class="logo-img">
+    </a>
 
-    <template v-slot:append>
-      <v-btn variant="tonal" color="primary" class="mx-10">Login</v-btn>
-    </template>
+    <!-- Spacer to push icons to the right -->
+    <v-spacer></v-spacer>
 
+    <!-- Navigation Buttons -->
+    <v-btn text @click="navigate('about-us')" class="mx-1">About Us</v-btn>
+    <v-btn text @click="navigate('services')" class="mx-1">Services</v-btn>
+    <v-btn text @click="navigate('contact')" class="mx-1">Contact</v-btn>
+
+    <!-- Login Button -->
+    <v-btn variant="outlined" color="secondary" class="mr-5 ml-1">Login</v-btn>
   </v-app-bar>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const navigate = (route) => {
+  // Implement navigation based on the route (e.g., using Vue Router)
+  console.log('Navigating to:', route);
+  // Example: Navigate to the route
+  router.push({ path: `/${route}` });
+};
 </script>
+
+<style scoped>
+.logo-img {
+  max-height: 120px; /* Set the maximum height to 80px */
+  max-width: 120px; /* Set the maximum width to 80px */
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="bg-contrast min fill-height">
-    <div class="pt-10 d-flex justify-center">
-      <Exercise class=""></Exercise>
-      <h1 class="text-h1">Start building your workout routines.</h1>
-    </div>
-
+  <div class="bg-contrast min fill-height d-flex flex-column justify-center align-center">
+    <!-- <Exercise class=""></Exercise> -->
+    <h1 class="text-h1">Start building your workout routines.</h1>
   </div>
 </template>
 
 <script setup>
-  import Exercise  from '@/components/Exercise.vue'
+import Exercise from '@/components/Exercise.vue';
 </script>
