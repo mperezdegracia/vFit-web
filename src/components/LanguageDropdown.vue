939f1a86bf9fe7136c3b62920bfc49afb06962d7
PakiQuian <f.quianblanco@gmail.com>
2023-10-04 16:01:23 -0300

    Creacion de tabs para settings

---
+++ src/components/LanguageDropdown.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-select
+      v-model="selectedLanguage"
+      :items="languages"
+      label="Select Language"
+      @change="changeLanguage"
+      :style="{'min-width': '200px'}"
+    ></v-select>
+</template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        selectedLanguage: '', // Initially selected language
+        languages: [
+          'English',
+          'Spanish',
+          'French',
+          // Add more languages as needed
+        ],
+      };
+    },
+    methods: {
+      changeLanguage() {
+        // You can handle the language change event here
+        console.log('Selected Language:', this.selectedLanguage);
+        // You can also use an event bus or a store to propagate the language change to other components.
+      },
+    },
+  };
+  </script>
+  
