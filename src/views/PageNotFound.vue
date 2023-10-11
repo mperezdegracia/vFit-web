32469c65059eaa7699b3f395d35ef1dc9a601621
Francisco Quian Blanco <f.quianblanco@gmail.com>
2023-10-11 10:57:03 -0300

    404 Page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="notfound">
+    <h2>404 Not found. The page you are looking for does not exist.</h2>
+    <h2><a href="/">To Landing Page</a></h2>
+  </div>
+</template>
+
+<style scoped>
+.notfound {
+  text-align: center;
+  margin: 20px;
+}
+</style>
