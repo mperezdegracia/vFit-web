ad62a22e9f971942b1fdd774030f244fd67e339b
stonefeld <theo.stanfield@gmail.com>
2023-10-11 01:27:21 -0300

    Agregado dinamico de ejercicios

--- src/views/RoutineCreate.vue
+++ src/views/RoutineCreate.vue
@@ -2,7 +2,7 @@
   <div class="bg-contrast min h-screen flex-column pt-4">
     <v-sheet class="mx-auto" max-width="800">
       <v-container class="bg-surface-variant">
-        <h3>Describe us your rutine</h3>
+        <h3>Describe us your routine</h3>
       </v-container>
       <v-container class="px-16">
         <v-text-field
@@ -37,14 +37,46 @@
       <v-container class="px-16">
         <div class="text-subtitle-2 mt-4 mb-2">Ejercicios seleccionados</div>
         <v-expansion-panels variant="inset" class="my-4">
-          <v-expansion-panel
-            v-for="ejercicio in ejercicios"
-            :key="ejercicio"
-          >
-            <v-expansion-panel-title>{{ ejercicio.nombre }}</v-expansion-panel-title>
-            <v-expansion-panel-text>{{ ejercicio.nombre }} - {{ ejercicio.repeticiones }}</v-expansion-panel-text>
+          <v-expansion-panel v-for="ejercicio in ejercicios" :key="ejercicio">
+            <v-expansion-panel-title class="bg-grey-lighten-2">{{
+              ejercicio.nombre
+            }}</v-expansion-panel-title>
+            <v-expansion-panel-text
+              >{{ ejercicio.nombre }} -
+              {{ ejercicio.repeticiones }}</v-expansion-panel-text
+            >
           </v-expansion-panel>
         </v-expansion-panels>
+        <div class="text-center">
+          <v-btn color="primary" block
+            >Add an exercise
+            <v-dialog v-model="dialog" activator="parent" width="auto">
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5"> Add an exercise </span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col>
+                        <v-text-field
+                          v-model="exerciseName"
+                          label="Exercise name"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" @click="dialog = false">Cancel</v-btn>
+                  <v-btn color="primary" @click="addEjercicio">Add</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-btn>
+        </div>
       </v-container>
     </v-sheet>
   </div>
@@ -55,6 +87,7 @@ import RoutineCard from "@/components/RoutineCard.vue";
 
 export default {
   data: () => ({
+    dialog: false,
     valid: false,
     title: "",
     titleRules: [
@@ -76,6 +109,7 @@ export default {
       4: "Hard",
       5: "Really hard",
     },
+    exerciseName: "",
 
     ejercicios: [
       {
@@ -90,8 +124,14 @@ export default {
         nombre: "Sentadillas",
         repeticiones: 3,
       },
-      ],
+    ],
   }),
+  methods: {
+    addEjercicio() {
+      this.ejercicios.push({ nombre: this.exerciseName, repeticiones: 10 });
+      this.dialog = false;
+    },
+  },
   components: {
     RoutineCard,
   },
