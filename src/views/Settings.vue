99ac22d7221504f5119bfa6f49551c23f3f07920
Francisco Quian Blanco <f.quianblanco@gmail.com>
2023-10-13 03:03:22 -0300

    Fix settings-bug

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,109 +1,160 @@
 <template>
-  <v-card class="h-100">
+  <v-sheet>
     <div class="d-flex flex-row">
       <v-tabs v-model="tab" direction="vertical" color="primary">
         <v-tab value="general">
           <v-icon start>mdi-cog</v-icon>
           General
         </v-tab>
+
         <v-tab value="profile">
           <v-icon start>mdi-account</v-icon>
           User Profile
         </v-tab>
+
         <v-tab value="notifications">
           <v-icon start>mdi-bell</v-icon>
           Notifications
         </v-tab>
+
         <v-tab value="privacy">
           <v-icon start>mdi-lock</v-icon>
           Privacy
         </v-tab>
+
         <v-tab value="language">
           <v-icon start>mdi-flag-variant</v-icon>
           Language
         </v-tab>
       </v-tabs>
-      <v-divider vertical></v-divider>
       <v-window v-model="tab">
-        <v-window-item value="profile">
-          <v-card flat>
+        <v-window-item value="general">
+          <v-sheet flat>
             <v-card-text>
-              <v-row>
-                <v-col cols="6" class="ml-3">
-                  <ul>
-                    <li>Username</li>
-                    <li>Email</li>
-                    <li>Change Password</li>
-                    <li>Friends</li>
-                    <li>Verify Account</li>
-                    <li>Notifications</li>
-                  </ul>
-                </v-col>
-                <v-col cols="6" class="ml-3">
-                  <ul>
-                    <li>Eddie Hall</li>
-                    <li>Email</li>
-                    <li>Change Password</li>
-                    <li>Friends</li>
-                    <li>Verify Account</li>
-                    <li>Notifications</li>
-                  </ul>
-                </v-col>
-              </v-row>
+              <p>
+                Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi
+                ac felis. Etiam feugiat lorem non metus. Sed a libero.
+              </p>
+
+              <p>
+                Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel,
+                lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis
+                non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non
+                nisl sit amet velit hendrerit rutrum.
+              </p>
+
+              <p class="mb-0">
+                Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu.
+                Pellentesque libero tortor, tincidunt et, tincidunt eget, semper
+                nec, quam. Phasellus blandit leo ut odio.
+              </p>
             </v-card-text>
-          </v-card>
+          </v-sheet>
+        </v-window-item>
+        <v-window-item value="profile">
+          <v-sheet flat>
+            <v-card-text>
+              <p>
+                Morbi nec metus. Suspendisse faucibus, nunc et pellentesque
+                egestas, lacus ante convallis tellus, vitae iaculis lacus elit
+                id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum
+                aliquam libero, non adipiscing dolor urna a orci. Curabitur
+                ligula sapien, tincidunt non, euismod vitae, posuere imperdiet,
+                leo. Nunc sed turpis.
+              </p>
+
+              <p>
+                Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque
+                egestas, lacus ante convallis tellus, vitae iaculis lacus elit
+                id tortor. Proin viverra, ligula sit amet ultrices semper,
+                ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In
+                hac habitasse platea dictumst. Fusce ac felis sit amet ligula
+                pharetra condimentum.
+              </p>
+
+              <p>
+                Sed consequat, leo eget bibendum sodales, augue velit cursus
+                nunc, quis gravida magna mi a libero. Nam commodo suscipit quam.
+                In consectetuer turpis ut velit. Sed cursus turpis vitae tortor.
+                Aliquam eu nunc.
+              </p>
+
+              <p>
+                Etiam ut purus mattis mauris sodales aliquam. Ut varius
+                tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce
+                fermentum odio nec arcu.
+              </p>
+
+              <p class="mb-0">
+                Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In
+                dui magna, posuere eget, vestibulum et, tempor auctor, justo.
+                Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed,
+                iaculis a, condimentum nec, nisi.
+              </p>
+            </v-card-text>
+          </v-sheet>
+        </v-window-item>
+        <v-window-item value="notifications">
+          <v-sheet flat>
+            <v-card-text>
+              <p>
+                Fusce a quam. Phasellus nec sem in justo pellentesque facilisis.
+                Nam eget dui. Proin viverra, ligula sit amet ultrices semper,
+                ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In
+                dui magna, posuere eget, vestibulum et, tempor auctor, justo.
+              </p>
+
+              <p class="mb-0">
+                Cras sagittis. Phasellus nec sem in justo pellentesque
+                facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut,
+                mi. Donec quam felis, ultricies nec, pellentesque eu, pretium
+                quis, sem. Nam at tortor in tellus interdum sagittis.
+              </p>
+            </v-card-text>
+          </v-sheet>
         </v-window-item>
         <v-window-item value="privacy">
-          <v-card flat>
+          <v-sheet flat>
             <v-card-text>
               <p>
-                Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nunc sed turpis.
+                Fusce a quam. Phasellus nec sem in justo pellentesque facilisis.
+                Nam eget dui. Proin viverra, ligula sit amet ultrices semper,
+                ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In
+                dui magna, posuere eget, vestibulum et, tempor auctor, justo.
               </p>
 
-              <p>
-                Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In hac habitasse platea dictumst. Fusce ac felis sit amet ligula pharetra condimentum.
-              </p>
-
-              <p>
-                Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Nam commodo suscipit quam. In consectetuer turpis ut velit. Sed cursus turpis vitae tortor. Aliquam eu nunc.
-              </p>
-
-              <p>
-                Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce fermentum odio nec arcu.
-              </p>
-
-              <p class="">
-                Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi.
+              <p class="mb-0">
+                Cras sagittis. Phasellus nec sem in justo pellentesque
+                facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut,
+                mi. Donec quam felis, ultricies nec, pellentesque eu, pretium
+                quis, sem. Nam at tortor in tellus interdum sagittis.
               </p>
             </v-card-text>
-          </v-card>
+          </v-sheet>
         </v-window-item>
         <v-window-item value="language">
-          <v-card flat>
+          <v-sheet flat>
             <v-card-text>
               <v-row>
-                <p class="ma-4">Language: </p>
-                <LanguageDropdown/>
+                <p class="ma-4">Language:</p>
+                <LanguageDropdown />
               </v-row>
             </v-card-text>
-          </v-card>
+          </v-sheet>
         </v-window-item>
       </v-window>
     </div>
-  </v-card>
+  </v-sheet>
 </template>
 
-<style scoped>
-</style>
-
 <script>
-  import LanguageDropdown from '@/components/LanguageDropdown.vue';
-  export default {
-    components: {
+import LanguageDropdown from "@/components/LanguageDropdown.vue";
+export default {
+  components: {
     LanguageDropdown,
   },
-    data: () => ({
-      tab: 'option-1',
-    }),
-  }
+  data: () => ({
+    tab: "option-1",
+  }),
+};
 </script>
