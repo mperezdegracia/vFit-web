973e2fe1c305730b17ebf1afdbe6a4354a77092b
Francisco Quian Blanco <f.quianblanco@gmail.com>
2023-10-13 01:50:16 -0300

    Update to page 404

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="notfound">
-    <h2>Error 404. The page you are looking for does not exist.</h2>
-    <h2><a href="/">To Landing Page</a></h2>
+  <div fluid class="ma-8">
+    <v-row align="center" justify="center">
+      <v-col cols="4" align="center" justify="center">
+        <v-img :width="350" src="/lost0.svg" />
+      </v-col>
+      <v-col cols="4" align="center" justify="center">
+        <div class="mt-4">
+          <h1 class="title text-center">Error 404</h1>
+          <h1 class="text-center">
+            The page you are looking for does not exist.
+          </h1>
+          <router-link to="/">
+            <v-btn color="primary" class="ma-4"> To landing page </v-btn>
+          </router-link>
+        </div>
+      </v-col>
+      <v-col cols="4" align="center" justify="center">
+        <v-img :width="350" src="/lost1.svg"></v-img>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <style scoped>
-.notfound {
-  text-align: center;
-  margin: 20px;
+.title {
+  font-size: 50px;
+  color: blueviolet;
 }
 </style>
