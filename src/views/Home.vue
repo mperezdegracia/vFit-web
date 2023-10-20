7d404d5bc1de2dcd0b8f33f1ff066084a766ed87
Francisco Quian Blanco <f.quianblanco@gmail.com>
2023-10-20 03:41:36 -0300

    💥Detonación de home💥 (No hay mas warnings ⚠ )

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,17 +4,39 @@
       <SideBar />
     </v-col>
 
-    <v-col md="10">
-      <h1 class="text-h2 pt-10 text-primary text-center font-weight-bold">
-        ¡Bienvenido!
-      </h1>
-      <div class="search-div">
-        <v-responsive class="search-container" max-width="60%">
+    <v-col>
+      <v-row align="center" justify="center" class="ma-3">
+        <h1 class="text-h2 text-primary text-center font-weight-bold mr-4">
+          ¡Bienvenido!
+        </h1>
+        <v-menu>
+          <template v-slot:activator="{ props: menu }">
+            <v-tooltip location="top">
+              <template v-slot:activator="{ props: tooltip }">
+                <v-btn
+                  color="primary"
+                  icon="mdi-order-bool-descending"
+                  v-bind="mergeProps(menu, tooltip)"
+                ></v-btn>
+              </template>
+              <span>Ordenar por</span>
+            </v-tooltip>
+          </template>
+
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-row>
+      <v-sheet class="bg-contrast mt-10" align="center" justify="center">
+        <v-responsive class="search-container" max-width="600">
           <v-text-field
             class="search-field"
             v-model="search"
             id="search"
-            label="Search"
+            label="Buscar"
             variant="solo"
             outlined
             dense
@@ -32,8 +54,8 @@
             </template>
           </v-text-field>
         </v-responsive>
-      </div>
-      <div v-if="searchResult.length === 0">
+      </v-sheet>
+      <div v-if="searchResult.length == 0">
         <v-row align="center" justify="center" class="mt-4 mb-10">
           <v-col md="3" sm="6" align="center" justify="center">
             <v-card class="rounded-xl elevation-4 ma-4 pb-3" to="/my-routines">
@@ -58,27 +80,19 @@
             </v-card>
           </v-col>
         </v-row>
-        <v-divider></v-divider>
-        <div>
-          <h1
-            class="text-center text-h3 text-primary slide-title ma-5 subtitles"
-          >
-            Recomendadas
-          </h1>
-          <div class="d-flex justify-center flex-scroll-container">
-            <div class="scroll-container ma-0">
-              <RoutineScroll :routines="routines"></RoutineScroll>
-            </div>
-          </div>
-        </div>
       </div>
       <div v-else>
-        <h1
-          class="w-50 text-center text-h4 text-secondary slide-title pt-5"
-          id="searchResultLabel"
-        >
-          {{ searchResultLabel }}
-        </h1>
+        <v-row>
+          <v-col>
+            <h1
+              class="w-50 text-center text-h4 text-secondary"
+              id="searchResultLabel"
+            >
+              {{ searchResultLabel }}
+            </h1>
+          </v-col>
+          <v-col cols="2"> </v-col>
+        </v-row>
         <!-- <div class="d-flex flex-container mx-auto">
               <RoutineCard
                 v-for="routine in searchResult"
@@ -92,151 +106,71 @@
   </v-row>
 </template>
 
-<script setup>
+<script>
 import RoutineScroll from "@/components/RoutineScroll.vue";
 import RoutineGrid from "@/components/RoutineGrid.vue";
 import SideBar from "@/components/SideBar.vue";
 import routines from "@/data/mockRoutines.js";
-import { ref } from "vue";
+import { ref, mergeProps } from "vue";
 
-const name = "SearchBar"; // Asigna el nombre de componente a una constante
-let search = ""; // Cambia "data" a "let" para definir una variable local
-let searchResult = ref("");
-let searchResultLabel = ""; // Texto del h1 de searchResults
-const searchResultLabelId = "searchResultLabel";
+export default {
+  data: () => ({
+    search: "", // Cambia "data" a "let" para definir una variable local
+    searchResult: ref(""),
+    searchResultLabel: "", // Texto del h1 de searchResults
+    searchResultLabelId: "searchResultLabel",
+    items: [
+      { title: "Fecha de creación" },
+      { title: "Puntuación" },
+      { title: "Dificultad" },
+      { title: "Categoría" },
+    ],
+  }),
+  methods: {
+    mergeProps,
 
-const searchItems = () => {
-  if (search.length > 0) {
-    // Perform search logic here
-    console.log(`Searching for ${search.toLowerCase()}`);
-    // hacer la logica del search
-    searchResult.value = routines;
+    searchItems() {
+      if (this.search.length > 0) {
+        // Perform search logic here
+        console.log(`Searching for ${this.search.toLowerCase()}`);
+        // hacer la logica del search
+        this.searchResult.value = routines;
 
-    // No se xq la primera vez no me cambia el txt
-    changeText(search);
+        // No se xq la primera vez no me cambia el txt
+        changeText(this.search);
 
-    return { searchResult };
-  }
-};
+        return this.searchResult;
+      }
+    },
 
-const constantSearch = () => {
-  if (search.length > 3) {
-    searchItems();
-  }
-};
+    constantSearch() {
+      if (this.search.length > 3) {
+        searchItems();
+      }
+    },
 
-const scrollToLiked = () => {
-  const likedSection = document.getElementById("liked-section");
-  if (likedSection) {
-    likedSection.scrollIntoView({ behavior: "smooth" });
-  }
-};
+    scrollToLiked() {
+      const likedSection = document.getElementById("liked-section");
+      if (likedSection) {
+        likedSection.scrollIntoView({ behavior: "smooth" });
+      }
+    },
 
-const changeText = (search) => {
-  searchResultLabel = "Results for '" + search + "'";
-  const searchResultLabel1 = document.getElementById("searchResultLabel");
-  searchResultLabel1.textContent = searchResultLabel;
-};
+    changeText(search) {
+      this.searchResultLabel = "Results for '" + search + "'";
+      const searchResultLabel1 = document.getElementById("searchResultLabel");
+      searchResultLabel1.textContent = this.searchResultLabel;
+    },
 
-const clearSearch = () => {
-  searchResult.value = "";
-  search = "";
+    clearSearch() {
+      this.searchResult.value = "";
+      this.search = "";
+    },
+  },
+  components: {
+    SideBar,
+    RoutineScroll,
+    RoutineGrid,
+  },
 };
 </script>
-
-<style>
-.new-button {
-  position: absolute;
-  margin-left: 62%;
-  min-height: 45px;
-  margin-bottom: 18px;
-  opacity: 90%;
-}
-
-.action-card:hover {
-  background-color: #f7f7f7;
-}
-
-.action-card {
-  background-color: white;
-  transition: background-color 0.1s ease-in-out, color 0.2s ease-in-out;
-}
-
-.action-card:active {
-  background-color: white;
-}
-
-.divider {
-  width: 90%;
-}
-
-.subtitles {
-  margin-bottom: -50px;
-}
-
-.scroll-container {
-  margin-top: 40px;
-  align-items: center;
-  width: 80%;
-}
-
-.slide-title {
-  font-family: "Courier New", Courier, monospace;
-}
-
-.search-div {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 20px;
-}
-
-@media (max-width: 600px) {
-  .search-div {
-    display: block;
-  }
-
-  .divider {
-    width: 90%;
-    margin-top: 50px !important;
-  }
-
-  .search-container {
-    min-width: 80%;
-    margin: auto;
-  }
-}
-
-.search-field {
-  min-width: 40%;
-}
-
-.flex-container {
-  display: flex;
-  flex-wrap: wrap;
-  /* Permite que los elementos se envuelvan a la siguiente línea cuando no haya suficiente espacio */
-  justify-content: center;
-  /* Distribuye el espacio disponible de manera uniforme entre los elementos */
-}
-
-/* Establece un ancho específico para cada elemento para que haya 3 por fila */
-.flex-container .routine-card {
-  width: calc(33.33% - 10px);
-  /* Resta 10px de margen para que haya espacio entre los elementos */
-  margin-bottom: 20px;
-  /* Espacio entre las filas */
-}
-
-.my-custom-button {
-  color: black;
-  /* Utiliza el color heredado para el icono */
-}
-
-.custom-append-icon {
-  position: absolute;
-  top: 50%;
-  right: 25px;
-  /* Ajusta la distancia desde el borde derecho del campo de texto */
-  transform: translateY(-90%);
-}
-</style>
